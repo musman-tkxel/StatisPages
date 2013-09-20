b87aaa779f98b00446fe57eb63e2e7c3af34e285
Muhammad Usman <muhammad.usman@tkxel.com>
2013-09-20 11:18:42 +0500

    View staticpage:contact updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 StaticPages::Application.routes.draw do
+  get "staticpage/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
